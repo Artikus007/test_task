89093a0093796e2e8a7b8ec288ed9aa8c1a28e0a
Vladyslav Artamonov <artamonovvv18@gmail.com>
2023-04-12 01:38:37 +0300

    Fixed issues

--- app/src/main/java/com/test/task/EventBroadcastReceiver.kt
+++ app/src/main/java/com/test/task/EventBroadcastReceiver.kt
@@ -3,14 +3,23 @@ package com.test.task
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.util.Log
 import com.test.task.utils.TAG
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class EventBroadcastReceiver : BroadcastReceiver() {
+class EventBroadcastReceiver
+@Inject
+constructor() : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         Log.d(TAG, "onReceive")
+    }
+
+    fun register(context: Context) {
+        context.registerReceiver(this, IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION))
     }
 }
--- app/src/main/java/com/test/task/MainActivity.kt
+++ app/src/main/java/com/test/task/MainActivity.kt
@@ -3,11 +3,26 @@ package com.test.task
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var receiver: EventBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        receiver.register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
